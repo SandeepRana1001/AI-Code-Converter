191346fba8187b1540627ec8aa101437681b36c4
Rana <sandrana@deloitte.com>
2023-04-21 12:59:54 +0530

    Added prompt

--- index.js
+++ index.js
@@ -20,6 +20,41 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration)
 
+app.post('/convert', async (req, res, next) => {
+
+    // const { prompt } = req.body
+    try {
+        const response = await openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: `
+                    const example = (arr)=>{
+                        arr.map((item)=>{
+                            console.log(item)
+                        });
+                    };
+
+                    The time complexity of this function is 
+                    ###
+            `,
+            max_tokens: 64,
+            temperature: 0,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+            stop: ["\n"],
+        })
+
+        console.log(response.data.choices)
+        return res.send('Ok')
+        // return res.status(200).json({
+        //     success: true,
+        //     data: response
+        // })
+    } catch (err) {
+        console.log(err)
+    }
+})
+
 
 const PORT = process.env.PORT || 8081
 app.listen(PORT, () => {
